b4b7563aacf8d7afcd7097290f9484d6b3aa81e1
Thomas RICHARD <thomas.richard.pro@gmail.com>
2011-12-06 16:10:35 +0100

    Mise-à-jour du main pour prendre en compte des nouvelles fonctions

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,11 +7,14 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-//*
+	// DÉCLARATION DES VARIABLES
     formule *une_formule;
     string lue;
     map<string, int> correspondance;
+    forme_conjonctive fc;
+    char *valeurs = NULL;
     
+    // LECTURE DE LA FORMULE
     if (argc >= 2) {
         lue = string(argv[1]);
         une_formule = litFormuleFichier(argv[1]);
@@ -21,9 +24,21 @@ int main(int argc, char** argv) {
         lue = buffer.str();
         une_formule = litFormuleString(lue);
     }
+    
     cout << "Analyse de " << lue << endl;
     cout << formule2string(une_formule) << endl;
+    
+    // NUMÉROTATION DES VARIABLES
     numerote(une_formule, correspondance);
-//*/
-return 0;
+    // TRADUCTION EN FORME CONJONCTIVE
+	fc = trad_forme_conjonctive(une_formule, correspondance);
+	// EXPLORATION DE L'ESPACE DE RECHERCHE
+	valeurs = new char[correspondance.size()];
+	cherche(fc, 0, valeurs);
+	
+	// LIBÉRATION DES ALLOCATIONS
+	delete[] valeurs;
+	detruire_formule(une_formule);
+	
+	return 0;
 }
